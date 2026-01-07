2cdd1a7e208621f3f5482a55ef96dbdc69491e76
SanmeetSingh21 <sanmeetsingh2121@gmail.com>
2026-01-07 16:17:37 +0530

    fix: remove BrowserRouter from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,40 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Navbar from "./components/Navbar"
-import Footer from "./components/Footer"
-import Layout from "./components/Layout"
+import { Routes, Route } from "react-router-dom";
 
-import Home from "./pages/Home"
-import About from "./pages/About"
-import Services from "./pages/Services"
-import Contact from "./pages/Contact"
-import Demo from "./pages/Demo"
-import AttachCar from "./pages/AttachCar"
-import Fleet from "./pages/Fleet"
-import ScrollToTop from "./components/ScrollToTop"
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Layout from "./components/Layout";
+import ScrollToTop from "./components/ScrollToTop";
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Contact from "./pages/Contact";
+import Demo from "./pages/Demo";
+import AttachCar from "./pages/AttachCar";
+import Fleet from "./pages/Fleet";
 
 function App() {
   return (
-    <BrowserRouter>
-    <ScrollToTop />
+    <>
+      <ScrollToTop />
+
       <Navbar />
+
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/services" element={<Services />} />
+          <Route path="/fleet" element={<Fleet />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/demo" element={<Demo />} />
           <Route path="/attach-car" element={<AttachCar />} />
-          <Route path="/fleet" element={<Fleet />} />
         </Routes>
       </Layout>
+
       <Footer />
-    </BrowserRouter>
-  )
+    </>
+  );
 }
 
-export default App
+export default App;
